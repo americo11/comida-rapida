2ff4984f48c71e82582fa56a6fedb3b5705e432e
AMERICO <ame.morales@alumnos.duoc.cl>
2020-11-14 12:55:46 -0300

    probando suerte

--- base/settings.py
+++ base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = 'dup1s86+ha&n6g&nlzffc!jm)45)xr6y&k5&bj5(n70zha5pse'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1','localhost'
+]
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalogo'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +80,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':  os.path.join( BASE_DIR , 'db.lunchestesi'),
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 # Create your models here.
+class Genero(models.Model):
+    nombre = models.CharField( max_length=200 , help_text='ingrese el nombre??¡???')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido =models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField('fecha de nacimeinto ', null=True, blank=True)
+    fecha_muerte = models.DateField('fecha de fallecimientyo ', null=True ,blank = True)
+
+
+class meta:
+    ordering =['apellido', 'nombre']
+
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
+
+    def get_absolute_url(self):
+        return reverse("autor-detalle", args=[str(self.id)])
+        
+
+
+
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)
+    resumen = models.TextField(max_length=200, help_text='ingresa brebe descripcion' )
+    isbn =  models.CharField('ISBN', max_length=13, help_text='15 cacracteres ')
+    genero = models.ManyToManyField('genero' , help_text='seeleccione el genero ')
+
+    def __str__(self):
+        return self.titulo
+
+
+    def get_absolute_url(self):
+        return reverse("libro-detalle", args=[str(self.id)])
+
+
+
+
+class idioma(models.Model):
+    nombre= models.CharField( max_length=50 , help_text='ingrese el idioma')
+
+    def __str__(self):
+        return self.nombre
+    
+
+
+
+    
+
+
+class inventario(models.Model):
+    ESTADO_PRESTAMO = (
+('m', 'Mantenimiento'),
+('p', 'prestamo'),
+('d', 'disponible'),
+('r', 'reservado')
+)
+
+    id  = models.UUIDField(primary_key=True , default=uuid.uuid4, help_text='unico')
+    #id = models.UUIDField(primary_Key=True, default=uuid.uuid4, help_text=' unico para este comentario ')
+    Libro = models.ForeignKey(Libro, on_delete=models.SET_NULL, null=True)
+#   libro = models.ForeignKey(libro, on_delete=models.SET_NULL, null=True)
+    idioma = models.ForeignKey(idioma, on_delete=models.SET_NULL, null=True)
+    impreso = models.CharField(max_length=200)
+    devolucion = models.DateField(null=True, blank=True)
+    estatus = models.CharField(max_length=10, choices=ESTADO_PRESTAMO,blank=True, default='m' , help_text='disponibilidad ')
+
+
+
+class meta:
+    ordering: ['devolucion  '] 
+
+    def __str__(self):
+        return f'{self.id (self.libro.titulo)}'
+    
